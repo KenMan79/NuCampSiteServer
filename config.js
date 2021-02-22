cf504add9f0d93f9247cb798ab5050591c97158d
Kenneth Luster <51566205+KenMan79@users.noreply.github.com>
2021-02-22 12:55:55 -0600

    Week 4 Workshop Nodejs

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '564164404541100',
+        clientSecret: '9aa53a480839921d2617225148cfb00a'
+    }
+}
